119f4b39b71007966e044c7ff94ce2dedf1bd461
JulioBarahona <julio.fbm96@gmail.com>
2016-08-19 14:58:02 -0600

    added defensive in main
    new class that validates user input,

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
-
 /**
  * @author Rafael Leon, Julio Barahona
  * Main de ejecucion de la calculadora PosFix
  */
 public class Main {
-	
+
+
 	public static void main(String []args)
 	{
+		//make new validation object
+		Validation newInput = new Validation();
+
+		//validation of user input
+		newInput.userInput();
+
 		CalcuPost calcu= new CalcuPost();
 		//String cadena= "8 2 / 2 + 3 -";
 		
